dd67d7c2f2dc6029fb5c397212c44cb2cb17860c
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-10-22 21:44:30 +0900

    コメント書いた

--- src/xoroshiro.h
+++ src/xoroshiro.h
@@ -14,20 +14,43 @@
 #include <ostream>
 
 namespace xoroshiro {
+
+/** Namespace containing implementation details */
 namespace detail {
 
+/** rotate right (64bit uint)
+ * @param x rotate this value
+ * @param k rotate k bits
+ * @return rotated value
+ */
 inline std::uint64_t rotl(const uint64_t x, int k) {
   return (x << k) | (x >> (64 - k));
 }
+
+/** create seed value#0
+ * @param s source of seed
+ * @return seed value
+ * @note This is an nfounded calculation.
+ */
 inline std::uint64_t conv_seed0(std::uint64_t s) {
   return (11660880167232069161ull * s) ^ 13827146352468370501ull;
 }
+
+/** create seed value#1
+ * @param s source of seed
+ * @return seed value
+ * @note This is an nfounded calculation.
+ */
 inline std::uint64_t conv_seed1(std::uint64_t s) {
   return (11559677109961209133ull * rotl(s, 32)) ^ 17519439474968054641ull;
 }
 
+/** base class of xoroshiro128++ and xoroshiro128**
+ * @tparam derived derived class
+ */
 template <typename derived> class rng128base {
 public:
+  /** The type of the generated random value. */
   using result_type = std::uint64_t;
 
 private:
@@ -39,21 +62,41 @@ private:
   }
 
 public:
+  /** the smallest possible value in the output range. */
   static constexpr result_type min() { return 0; }
 
+  /** the largest possible value in the output range. */
   static constexpr result_type max() { return ~(min()); }
 
+  /** compares two rng objects of the same type for equality.
+   * @param a rng object.
+   * @param b another rng object.
+   * @returns true if a and b generates same values.
+   */
   inline friend //
       bool
       operator==(derived const &a, derived const &b) {
     return a.same_to(b);
   }
+
+  /** compares two rng objects of the same type for equality.
+   * @param a rng object.
+   * @param b another rng object.
+   * @returns false if a and b generates same values.
+   */
   inline friend //
       bool
       operator!=(derived const &a, derived const &b) {
     return !a.same_to(b);
   }
 
+  /** read state of rng from input stream
+   * @tparam CharT a template parameter to specify stream class
+   * @tparam Traits a template parameter to specify stream class
+   * @param is An input stream.
+   * @param rng read state from is and write to rng
+   * @returns The input stream with the state of rng extracted.
+   */
   template <typename CharT, typename Traits> //
   friend inline                              //
       std::basic_istream<CharT, Traits> &
@@ -64,6 +107,13 @@ public:
     return is;
   }
 
+  /** output current state of rng
+   * @tparam CharT a template parameter to specify stream class
+   * @tparam Traits a template parameter to specify stream class
+   * @param os an output stream
+   * @param rng output state of this value
+   * @return the output stream with the state of rng inserted.
+   */
   template <typename CharT, typename Traits> //
   friend inline                              //
       std::basic_ostream<CharT, Traits> &
@@ -74,22 +124,37 @@ public:
   }
 
 protected:
-  std::uint64_t s[2];
+  std::uint64_t s[2]; ///< inner rng state
 
+  /** compares two rng objects of the same type for equality.
+   * @param a rng object.
+   * @param b another rng object.
+   * @returns true if a and b generates same values.
+   */
   bool same_to(rng128base const &that) const {
     return s[0] == that.s[0] && s[1] == that.s[1];
   }
+
+  /** constructor with default seed. */
   rng128base() : s{default_seed0(), default_seed1()} {}
 
+  /** constructor with seed
+   * @param seed seed of rng.
+   */
   explicit rng128base(result_type seed)
       : s{detail::conv_seed0(seed), detail::conv_seed1(seed)} {}
 };
 
 } // namespace detail
+
+/** Xoroshiro128++ */
 class rng128pp : public detail::rng128base<rng128pp> {
   using base = detail::rng128base<rng128pp>;
 
 public:
+  /** create new random number and update inner state.
+   * @return new random number
+   */
   result_type operator()() {
     result_type const s0 = s[0];
     result_type s1 = s[1];
@@ -99,14 +164,24 @@ public:
     s[1] = detail::rotl(s1, 28);                   // c
     return result;
   }
+
+  /** constructor with seed
+   * @param seed seed of rng.
+   */
   explicit rng128pp(result_type seed) : base(seed) {}
+
+  /** constructor with default seed. */
   rng128pp() : base() {}
 };
 
+/** Xoroshiro128** */
 class rng128ss : public detail::rng128base<rng128ss> {
   using base = detail::rng128base<rng128ss>;
 
 public:
+  /** create new random number and update inner state.
+   * @return new random number
+   */
   result_type operator()() {
     const uint64_t s0 = s[0];
     uint64_t s1 = s[1];
@@ -116,7 +191,13 @@ public:
     s[1] = detail::rotl(s1, 37);                   // c
     return result;
   }
+
+  /** constructor with seed
+   * @param seed seed of rng.
+   */
   explicit rng128ss(result_type seed) : base(seed) {}
+
+  /** constructor with default seed. */
   rng128ss() : base() {}
 };
 } // namespace xoroshiro
