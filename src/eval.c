9574a10e69bd36b4452b50d8869e834585503189
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-10-24 22:28:18 +0900

    xorshift128 が失敗するぐらいのテストにする

--- src/eval.c
+++ src/eval.c
@@ -2,12 +2,25 @@
 
 unif01_Gen *create_unif01();
 
+enum { NDIM = 200 };
+enum { CRUSH_NUM = 1 };
+
+void mid_crush(unif01_Gen *gen) {
+  // int Rep[1 + NDIM] = {0, 1, 1, 1};
+  int Rep[1 + NDIM] = {                    //
+                       10, 10, 10, 10, 10, //
+                       10, 10, 10, 10, 10, //
+                       10, 10, 10, 10, 10};
+
+  bbattery_RepeatSmallCrush(gen, Rep);
+}
+
 int main(void) {
 
   // Create TestU01 PRNG object for our generator
   unif01_Gen *gen = create_unif01();
 
   // Run the tests.
-  bbattery_SmallCrush(gen);
+  mid_crush(gen);
   return 0;
 }
