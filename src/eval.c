d5e00361ee35cf3c54287a5c75c7a49bd25f6991
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-10-25 13:17:06 +0900

    mid_crush を入れる

--- src/eval.c
+++ src/eval.c
@@ -5,15 +5,7 @@ unif01_Gen *create_unif01();
 enum { NDIM = 200 };
 enum { CRUSH_NUM = 1 };
 
-void mid_crush(unif01_Gen *gen) {
-  // int Rep[1 + NDIM] = {0, 1, 1, 1};
-  int Rep[1 + NDIM] = {                    //
-                       10, 10, 10, 10, 10, //
-                       10, 10, 10, 10, 10, //
-                       10, 10, 10, 10, 10};
-
-  bbattery_RepeatSmallCrush(gen, Rep);
-}
+void mid_crush(unif01_Gen *gen);
 
 int main(void) {
 
